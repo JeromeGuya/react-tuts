b645677cc877540a05faa3feb45d5b2812876d1e
Jerome <jerome.guya@evsu.edu.ph>
2025-03-19 19:55:33 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Order from "./Order";
+import PizzaOfTheDay from "./PizzaOfTheDay";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="logo">Pizza Store - Create Order</h1>
+      <Order />
+      <PizzaOfTheDay />
+    </div>
+  );
+}
